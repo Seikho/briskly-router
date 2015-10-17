07ce19d5c84f84389ee24d517e02abe8f10c5a33
Carl <carl@carlossus.com>
2015-10-17 18:59:20 +0800

    Add error definitions

---
+++ src/errors.js
@@ -0,0 +1,8 @@
+var errors = {
+    NoOptions: 'No route options provided',
+    NoMethod: 'No route method provided',
+    NoHandler: 'No route handler provided',
+    NoPath: 'No route path provided'
+};
+module.exports = errors;
+//# sourceMappingURL=errors.js.map
---
+++ src/errors.ts
@@ -0,0 +1,8 @@
+export = errors;
+
+var errors = {
+    NoOptions: 'No route options provided',
+    NoMethod: 'No route method provided',
+    NoHandler: 'No route handler provided',
+    NoPath: 'No route path provided'
+};
