540d1e725647b490e0ce8b9f4370a051c9a5e95d
Carl <carl@carlossus.com>
2015-10-17 19:08:22 +0800

    Implement basic exported api

---
+++ src/index.js
@@ -0,0 +1,5 @@
+exports.route = require('./route');
+exports.server = require('./server');
+exports.start = require('./start');
+exports.stop = require('./stop');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export import route = require('./route');
+
+export import server = require('./server');
+
+export import start = require('./start');
+
+export import stop = require('./stop');
