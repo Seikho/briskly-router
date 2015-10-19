7c5bb8dbac2ea6eaa7769d585cc09f62731f166a
Carl <carl@carlossus.com>
2015-10-19 18:37:55 +0800

    Export route parse function instead of peg parser

--- src/parsers/route.js
+++ src/parsers/route.js
@@ -3,5 +3,5 @@ var path = require('path');
 var PEG = require('pegjs');
 var grammar = fs.readFileSync(path.join(__dirname, 'route.peg')).toString();
 var parser = PEG.buildParser(grammar);
-module.exports = parser;
+module.exports = parser.parse;
 //# sourceMappingURL=route.js.map
--- src/parsers/route.ts
+++ src/parsers/route.ts
@@ -3,7 +3,7 @@ import fs = require('fs');
 import path = require('path');
 var PEG = require('pegjs');
 
-export = parser;
+export = parser.parse;
 
 var grammar = fs.readFileSync(path.join(__dirname, 'route.peg')).toString();
 var parser: Types.PegParser<Types.RoutePart[]> = PEG.buildParser(grammar);
