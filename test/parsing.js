7d6a244b290cc203e64741410e69596ea64608b3
Carl <carl@carlossus.com>
2015-10-20 22:14:48 +0800

    Simplify testType fixture

--- test/parsing.js
+++ test/parsing.js
@@ -5,50 +5,38 @@ describe('request parsing', function () {
     it('will return a single string part', function () {
         var parts = request('/a-string');
         expect(parts.length).to.equal(1);
-        expect(testType(parts[0], 'string', 'a-string')).to.be.true;
+        testType(parts[0], 'string', 'a-string');
     });
     it('will return a single number part', function () {
         var parts = request('/12345');
         expect(parts.length).to.equal(1);
-        expect(testType(parts[0], 'number', 12345)).to.be.true;
+        testType(parts[0], 'number', 12345);
     });
     it('will return a single array part', function () {
         var parts = request('/[1,2,3]');
         expect(parts.length).to.equal(1);
-        expect(testType(parts[0], 'array', [1, 2, 3])).to.be.true;
+        testType(parts[0], 'array', [1, 2, 3]);
     });
     it('will return a single object part', function () {
         var parts = request('/{ "a": true, "b": "blue" }');
         expect(parts.length).to.equal(1);
-        expect(testType(parts[0], 'object', { a: true, b: 'blue' })).to.be.true;
+        testType(parts[0], 'object', { a: true, b: 'blue' });
+    });
+    it('will return two parts, string and string', function () {
+        var parts = request('/first/second');
+        expect(parts.length).to.equal(2);
+        testType(parts[0], 'string', 'first');
+        testType(parts[1], 'string', 'second');
     });
 });
 function testType(part, type, value) {
-    if (part.type !== type) {
-        console.log("Expected type '" + type + "'. Actual: '" + part.type + "'");
-        return false;
-    }
-    if (type === 'string' || type === 'number') {
-        if (part.value !== value) {
-            console.log("Expected value '" + value + "'. Actual: '" + part.value + "'");
-            return false;
-        }
-        return true;
-    }
-    if (type === 'array') {
-        if (!arraysEqual(part.value, value)) {
-            console.log("Expected value: " + JSON.stringify(value));
-            console.log("Actual value: " + JSON.stringify(part.value));
-            return false;
-        }
-        return true;
-    }
-    if (!objectsEqual(part.value, value)) {
-        console.log("Expected value: " + JSON.stringify(value));
-        console.log("Actual value: " + JSON.stringify(part.value));
-        return false;
-    }
-    return true;
+    expect(part.type).to.equal(type);
+    if (part.type === 'string' || part.type === 'number')
+        expect(part.value).to.equal(value);
+    if (type === 'array')
+        expect(part.value).to.have.same.members(value);
+    if (type === 'object')
+        expect(part.value).to.deep.equal(value);
 }
 function arraysEqual(left, right) {
     return left.every(function (value, index) { return right[index] === value; });
--- test/parsing.ts
+++ test/parsing.ts
@@ -5,62 +5,48 @@ var expect = chai.expect;
 
 describe('request parsing', () => {
 
-    it('will return a single string part', () => {       
+    it('will return a single string part', () => {
         var parts = request('/a-string');
         expect(parts.length).to.equal(1);
-        expect(testType(parts[0], 'string', 'a-string')).to.be.true;
+        testType(parts[0], 'string', 'a-string');
     });
 
     it('will return a single number part', () => {
         var parts = request('/12345');
         expect(parts.length).to.equal(1);
-        expect(testType(parts[0], 'number', 12345)).to.be.true;
+        testType(parts[0], 'number', 12345);
     });
-    
+
     it('will return a single array part', () => {
-       var parts = request('/[1,2,3]');
-       expect(parts.length).to.equal(1);
-       expect(testType(parts[0], 'array', [1,2,3])).to.be.true; 
+        var parts = request('/[1,2,3]');
+        expect(parts.length).to.equal(1);
+        testType(parts[0], 'array', [1, 2, 3]);
     });
-    
+
     it('will return a single object part', () => {
-       var parts = request('/{ "a": true, "b": "blue" }');
-       expect(parts.length).to.equal(1);
-       expect(testType(parts[0], 'object', { a: true, b: 'blue'})).to.be.true; 
+        var parts = request('/{ "a": true, "b": "blue" }');
+        expect(parts.length).to.equal(1);
+        testType(parts[0], 'object', { a: true, b: 'blue' });
+    });
+
+    it('will return two parts, string and string', () => {
+        var parts = request('/first/second');
+        expect(parts.length).to.equal(2);
+        testType(parts[0], 'string', 'first');
+        testType(parts[1], 'string', 'second');
     });
 });
 
-function testType(part: Types.Part, type: string, value: any): boolean {
-    if (part.type !== type) {
-        console.log(`Expected type '${type}'. Actual: '${part.type}'`);
-        return false;
-    }
+function testType(part: Types.Part, type: string, value: any): void {
+    expect(part.type).to.equal(type);
+    if (part.type === 'string' || part.type === 'number')
+        expect(part.value).to.equal(value);
 
-    if (type === 'string' || type === 'number') {
-        if (part.value !== value) {
-            console.log(`Expected value '${value}'. Actual: '${part.value}'`);
-            return false;
-        }
-        return true;
-    }
+    if (type === 'array')
+        expect(part.value).to.have.same.members(value);
 
-    if (type === 'array') {
-        if (!arraysEqual(part.value, value)) {
-            console.log(`Expected value: ${JSON.stringify(value) }`);
-            console.log(`Actual value: ${JSON.stringify(part.value) }`);
-            return false;
-        }
-        return true;
-    }
-
-    if (!objectsEqual(part.value, value)) {
-        console.log(`Expected value: ${JSON.stringify(value) }`);
-        console.log(`Actual value: ${JSON.stringify(part.value) }`);
-        return false;
-    }
-
-
-    return true;
+    if (type === 'object')
+        expect(part.value).to.deep.equal(value);
 }
 
 function arraysEqual(left: any[], right: any[]) {
