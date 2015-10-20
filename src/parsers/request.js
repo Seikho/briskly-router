27d416ce1e7f13b0e0cb421487f82cb4048583d1
Carl <carl@carlossus.com>
2015-10-20 19:24:42 +0800

    Implement request parser

--- src/parsers/request.js
+++ src/parsers/request.js
@@ -1,4 +1,67 @@
-function parse() {
+function parse(request) {
+    var parts = request.split('/');
+    return parts.map(getTypes);
 }
-exports.parse = parse;
+function toNumber(input) {
+    if (input === 'Infinity' || input === '-Infinity')
+        return null;
+    if (isNaN(input))
+        return null;
+    return {
+        type: 'number',
+        value: Number(input)
+    };
+}
+function toString(input) {
+    return {
+        type: 'string',
+        value: input
+    };
+}
+function toArray(input) {
+    try {
+        var value = JSON.parse(input);
+        if (!Array.isArray(value))
+            return null;
+        return {
+            type: 'array',
+            value: value
+        };
+    }
+    catch (ex) {
+        return null;
+    }
+}
+function toObject(input) {
+    try {
+        var value = JSON.parse(input);
+        if (value instanceof Array)
+            return null;
+        if (value instanceof String)
+            return null;
+        if (value instanceof Number)
+            return null;
+        return {
+            type: 'object',
+            value: value
+        };
+    }
+    catch (ex) {
+        return null;
+    }
+}
+var casters = [toString, toNumber, toArray, toObject];
+function getTypes(part) {
+    var types = casters.reduce(function (prev, curr) {
+        var type = curr(part);
+        if (!type)
+            return prev;
+        return [type].concat(prev);
+    }, []);
+    var part = {
+        part: part,
+        types: types
+    };
+}
+module.exports = parse;
 //# sourceMappingURL=request.js.map
--- src/parsers/request.ts
+++ src/parsers/request.ts
@@ -1,10 +1,74 @@
 import Types = require('../../index.d.ts');
 export = parse;
 
-const WORD = /[a-zA-Z0-9]+/;
-const NUMBER = /[0-9]+/;
-
-
 function parse(request: string) {
-    return request.split('/');
+    var parts = request.split('/');
+
+    return parts.map(getTypes);
 }
+
+function toNumber(input: any) {
+    if (input === 'Infinity' || input === '-Infinity') return null;
+    if (isNaN(input)) return null;
+
+    return {
+        type: 'number',
+        value: Number(input)
+    };
+}
+
+function toString(input: any) {
+
+    return {
+        type: 'string',
+        value: input
+    };
+}
+
+function toArray(input: any) {
+    try {
+        var value = JSON.parse(input);
+        if (!Array.isArray(value)) return null;
+        return {
+            type: 'array',
+            value
+        };
+    }
+    catch (ex) {
+        return null;
+    }
+}
+
+function toObject(input: any) {
+    try {
+        var value = JSON.parse(input);
+        if (value instanceof Array) return null;
+        if (value instanceof String) return null;
+        if (value instanceof Number) return null;
+        return {
+            type: 'object',
+            value
+        };
+    }
+    catch (ex) {
+        return null;
+    }
+}
+
+var casters = [toString, toNumber, toArray, toObject];
+
+function getTypes(part: any) {
+
+    var types = casters.reduce((prev, curr) => {
+        var type = curr(part);
+        if (!type) return prev;
+
+        return [type].concat(prev);
+    }, []);
+
+    var part: any = {
+        part,
+        types
+    }
+}
+
