3c0b7bcfb7f70438f50c903b7e893478d43048e7
Carl Winkler <carl.winkler86@gmail.com>
2015-10-23 14:34:47 +0800

    WIP: Implement request/route part matching

--- src/match.js
+++ src/match.js
@@ -1,4 +1,7 @@
+var requestParser = require('./parsers/request');
 function match(path) {
+    var parts = requestParser(path);
+    // TODO: find matching routes
     var options = {
         method: 'GET',
         path: path,
@@ -8,6 +11,13 @@ function match(path) {
         }
     };
     return options;
+}
+function isMatch(request, route) {
+    if (route.type === 'route')
+        return request.part === route.part;
+    if (route.cast === 'any')
+        return true;
+    return route.cast === request.cast;
 }
 module.exports = match;
 //# sourceMappingURL=match.js.map
--- src/match.ts
+++ src/match.ts
@@ -1,10 +1,14 @@
 import Types = require('../index.d.ts');
 import routes = require('./routes');
 import log = require('ls-logger');
+import requestParser = require('./parsers/request');
 export = match;
 
 function match(path: string): Types.RouteOptions {
-    
+    var parts = requestParser(path);
+
+    // TODO: find matching routes
+        
     var options: Types.RouteOptions = {
         method: 'GET',
         path: path,
@@ -15,4 +19,14 @@ function match(path: string): Types.RouteOptions {
     }
     
     return options;
+}
+
+function isMatch(request: Types.RequestPart, route: Types.RoutePart) {
+    if (route.type === 'route')
+        return request.part === route.part;
+
+    if (route.cast === 'any')
+        return true;
+    
+    return route.cast === request.cast;
 }
