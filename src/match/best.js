c7bff6c52fe9ad9888e34b58d4506f8f8fe80321
Carl Winkler <carl.winkler86@gmail.com>
2015-10-28 13:14:59 +0800

    WIP: Implement best match logic

--- src/match/best.js
+++ src/match/best.js
@@ -1,4 +1,31 @@
+var compare = require('./compare');
+var routes = require('../routes');
 function bestMatch(request) {
+    var comparisons = routes
+        .map(function (route) { return compare(request, route); })
+        .filter(function (result) { return result != null; });
+    var matches = comparisons.slice();
+    for (var i = 0; i < request.parts.length; ++i) {
+        var exactMatches = [];
+        for (var comparator in matchPriority) {
+            exactMatches = matches
+                .filter(function (result) { return result[i] === comparator; });
+            if (exactMatches.length === 0)
+                continue;
+            // Return the outer loop
+            break;
+        }
+        if (exactMatches.length === 0)
+            if (matches.length > 0)
+                continue;
+    }
+    if (matches.length > 1) {
+    }
     return null;
 }
+var matchPriority = [
+    0 /* Part */,
+    1 /* Type */,
+    2 /* Any */
+];
 //# sourceMappingURL=best.js.map
--- src/match/best.ts
+++ src/match/best.ts
@@ -1,7 +1,41 @@
 import compare = require('./compare');
 import routes = require('../routes');
+import Match = Types.Match;
 
 function bestMatch(request: Types.Request): Types.Route {
+    var comparisons = routes
+        .map(route => compare(request, route))
+        .filter(result => result != null);
+
+    var matches = comparisons.slice();
+    for (var i = 0; i < request.parts.length; ++i) {
+        var exactMatches = [];
+        for (var comparator in matchPriority) {
+            exactMatches = matches
+                .filter(result => result[i] === comparator);
+            
+            if (exactMatches.length === 0) continue;
+            
+            // Return the outer loop
+            break;
+        }
+        
+        if (exactMatches.length === 0) 
+
+        if (matches.length > 0) continue;
+
+    }
+
+    if (matches.length > 1) {
+        // Ambiguous match
+    }
     
     return null;
 }
+
+var matchPriority = [
+    Match.Part,
+    Match.Type,
+    Match.Any
+];
+
