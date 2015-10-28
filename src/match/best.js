1cc9b01d1a00d1b554737d93363f754e4ad2db84
Carl <carl@carlossus.com>
2015-10-28 21:46:18 +0800

    Finish first impl of best request/route match

--- src/match/best.js
+++ src/match/best.js
@@ -3,25 +3,31 @@ var routes = require('../routes');
 function bestMatch(request) {
     var comparisons = routes
         .map(function (route) { return compare(request, route); })
-        .filter(function (result) { return result != null; });
+        .map(toComparison)
+        .filter(function (comparison) { return comparison.matches != null; });
     var matches = comparisons.slice();
     for (var i = 0; i < request.parts.length; ++i) {
+        var forMatches = function (comparator) { return function (comparison) { return comparison.matches[i] === comparator; }; };
         var exactMatches = [];
         for (var comparator in matchPriority) {
-            exactMatches = matches
-                .filter(function (result) { return result[i] === comparator; });
-            if (exactMatches.length === 0)
-                continue;
-            // Return the outer loop
-            break;
+            exactMatches = matches.filter(forMatches(comparator));
+            if (exactMatches.length !== 0)
+                break;
         }
+        // We must find matches during each iteration
         if (exactMatches.length === 0)
-            if (matches.length > 0)
-                continue;
+            return null;
+        matches = exactMatches.slice();
     }
     if (matches.length > 1) {
     }
-    return null;
+    return routes[matches[0].index];
+}
+function toComparison(matches, index) {
+    return {
+        index: index,
+        matches: matches
+    };
 }
 var matchPriority = [
     0 /* Part */,
--- src/match/best.ts
+++ src/match/best.ts
@@ -5,32 +5,39 @@ import Match = Types.Match;
 function bestMatch(request: Types.Request): Types.Route {
     var comparisons = routes
         .map(route => compare(request, route))
-        .filter(result => result != null);
+        .map(toComparison)
+        .filter(comparison => comparison.matches != null);
 
     var matches = comparisons.slice();
+
+
     for (var i = 0; i < request.parts.length; ++i) {
+        var forMatches = (comparator: Match) => (comparison: Comparison) => comparison.matches[i] === comparator;
         var exactMatches = [];
+
         for (var comparator in matchPriority) {
-            exactMatches = matches
-                .filter(result => result[i] === comparator);
-            
-            if (exactMatches.length === 0) continue;
-            
-            // Return the outer loop
-            break;
+            exactMatches = matches.filter(forMatches(comparator));
+            if (exactMatches.length !== 0) break;
         }
         
-        if (exactMatches.length === 0) 
-
-        if (matches.length > 0) continue;
-
+        // We must find matches during each iteration
+        if (exactMatches.length === 0) return null;
+        matches = exactMatches.slice();
     }
 
     if (matches.length > 1) {
-        // Ambiguous match
+        // Ambiguous match -- should not occur
+        // TODO: How to handle?
     }
-    
-    return null;
+
+    return routes[matches[0].index];
+}
+
+function toComparison(matches: Array<Match>, index: number) {
+    return {
+        index,
+        matches
+    };
 }
 
 var matchPriority = [
@@ -39,3 +46,7 @@ var matchPriority = [
     Match.Any
 ];
 
+interface Comparison {
+    index: number;
+    matches: Array<Match>;
+}
