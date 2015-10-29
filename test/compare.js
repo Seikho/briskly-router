217430688dc9ec90ccdbcc207850b36f446afacc
Carl <carl@carlossus.com>
2015-10-29 22:30:42 +0800

    Begin implementing bestMatch unit tests

--- test/compare.js
+++ test/compare.js
@@ -1,13 +1,40 @@
-var route = require('../src/parsers/route');
-var request = require('../src/parsers/request');
+var request = require('../src/match/request');
+var best = require('../src/match/best');
+var add = require('../src/add');
 var chai = require('chai');
 var expect = chai.expect;
 describe('request/route comparison tests', function () {
     it('will return a single Part match', function () {
-        var rt = route('/a-part');
+        addRoute('/a-part');
         var req = request('/a-part');
+        var match = best(req);
+        expect(match).to.exist;
+        expect(match.options.path).to.equal('/a-part');
+    });
+    it('will not return a match', function () {
+        var req = request('/a-sparta');
+        var match = best(req);
+        expect(match).to.not.exist;
+    });
+    it('will return a match based on string type', function () {
+        addRoute('/{word: string}');
+        var match = best(request('/a-sparta'));
+        expect(match).to.exist;
+        expect(match.options.path).to.equal('/{word: string}');
+    });
+    it('will return not return a match due to type mismatch', function () {
+        var match = best(request('/12345'));
+        expect(match).to.not.exist;
     });
 });
+function addRoute(path, method) {
+    method = method || 'get';
+    add({
+        method: method,
+        path: path,
+        handler: function () { }
+    });
+}
 function test(matches, expected) {
     expect(matches.length).to.equal(expected.length);
     matches.forEach(function (match, index) { return expect(match).to.equal(expected[index]); });
--- test/compare.ts
+++ test/compare.ts
@@ -1,6 +1,8 @@
 import compare = require('../src/match/compare');
 import route = require('../src/parsers/route');
-import request = require('../src/parsers/request');
+import request = require('../src/match/request');
+import best = require('../src/match/best');
+import add = require('../src/add');
 import * as chai from 'chai'
 import Match = Types.Match;
 var expect = chai.expect;
@@ -8,12 +10,42 @@ var expect = chai.expect;
 describe('request/route comparison tests', () => {
 	
 	it('will return a single Part match', () => {
-		var rt = route('/a-part');
-		var req = request('/a-part');
+		addRoute('/a-part');
+        var req = request('/a-part');                
+		var match = best(req);
+        expect(match).to.exist;
+        expect(match.options.path).to.equal('/a-part');        
 	});
+    
+    it('will not return a match', () => {
+       var req = request('/a-sparta');
+       var match = best(req);
+       expect(match).to.not.exist; 
+    });
+    
+    it('will return a match based on string type', () => {
+        addRoute('/{word: string}');
+        var match = best(request('/a-sparta'));
+        expect(match).to.exist;
+        expect(match.options.path).to.equal('/{word: string}');
+    });
+    
+    it('will return not return a match due to type mismatch', () => {
+       var match = best(request('/12345'));
+       expect(match).to.not.exist; 
+    });
 	
 	
 });
+
+function addRoute(path: string, method?: string) {
+    method = method || 'get';
+    add({
+        method,
+        path,
+        handler: () => {}
+    });
+}
 
 function test(matches: Array<Match>, expected: Array<Match>) {
 	expect(matches.length).to.equal(expected.length);
