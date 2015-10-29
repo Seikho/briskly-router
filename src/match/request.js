a86a78c7f00c22359083055509b3fb9a39de5857
Carl <carl@carlossus.com>
2015-10-29 22:27:59 +0800

    Default toRequest method to GET

--- src/match/request.js
+++ src/match/request.js
@@ -1,7 +1,7 @@
 var requestParser = require('../parsers/request');
 function toRequest(path, method) {
     var parts = requestParser(path);
-    method = method.toUpperCase();
+    method = (method || 'GET').toUpperCase();
     return {
         parts: parts,
         path: path,
--- src/match/request.ts
+++ src/match/request.ts
@@ -1,9 +1,9 @@
 import requestParser = require('../parsers/request');
 export = toRequest;
 
-function toRequest(path: string, method: string): Types.Request {
+function toRequest(path: string, method?: string): Types.Request {
 	var parts = requestParser(path);
-	method = method.toUpperCase();
+	method = (method || 'GET').toUpperCase();
     
 	return {
 		parts,
