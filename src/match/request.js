2cb8398ec9cf94affd857ea131eaad1cc42bd394
Carl <carl@carlossus.com>
2015-10-29 22:20:18 +0800

    convert request method to upperCase when converting to Request

--- src/match/request.js
+++ src/match/request.js
@@ -1,6 +1,7 @@
 var requestParser = require('../parsers/request');
 function toRequest(path, method) {
     var parts = requestParser(path);
+    method = method.toUpperCase();
     return {
         parts: parts,
         path: path,
--- src/match/request.ts
+++ src/match/request.ts
@@ -3,7 +3,8 @@ export = toRequest;
 
 function toRequest(path: string, method: string): Types.Request {
 	var parts = requestParser(path);
-	
+	method = method.toUpperCase();
+    
 	return {
 		parts,
 		path,
