29ddf2b3828c930c18e3aa7da0bbd2627aee3ee2
Carl <carl@carlossus.com>
2015-10-30 18:14:05 +0800

    Add invalid path and wildcard errors

--- src/errors.js
+++ src/errors.js
@@ -2,7 +2,9 @@ var errors = {
     NoOptions: 'No route options provided',
     NoMethod: 'No route method provided',
     NoHandler: 'No route handler provided',
-    NoPath: 'No route path provided'
+    NoPath: 'No route path provided',
+    InvalidPath: 'Invalid path provided',
+    InvalidWildCard: 'Invalid wildcard use: Wildcard can only be at the end of a path'
 };
 module.exports = errors;
 //# sourceMappingURL=errors.js.map
