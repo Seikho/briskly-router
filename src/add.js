b231a5c394617019230c6c10aa686d5a87c85e0c
Carl <carl@carlossus.com>
2015-10-31 12:15:10 +0800

    Disallow non-unique param names in route addition

--- src/add.js
+++ src/add.js
@@ -1,8 +1,13 @@
 var routes = require('./routes');
 var routeParser = require('./parsers/route');
+var errors = require('./errors');
 function add(options) {
     var parts = routeParser(options.path);
     options.method = options.method.toUpperCase();
+    var matchingParams = parts
+        .some(function (part) { return parts.filter(function (p) { return p.type != null && p.part === part.part; }).length > 1; });
+    if (matchingParams)
+        throw new Error(errors.MatchingParamNames);
     // TODO: Chcek for ambiguous routes
     routes.push({
         options: options,
--- src/add.ts
+++ src/add.ts
@@ -1,10 +1,17 @@
 import routes = require('./routes');
 import routeParser = require('./parsers/route');
+import errors = require('./errors')
 export = add;
 
 function add(options: Types.RouteOptions) {
     var parts = routeParser(options.path);
     options.method = options.method.toUpperCase();
+
+    var matchingParams = parts
+        .some(part => parts.filter(p => p.type != null && p.part === part.part).length > 1);
+    
+    if (matchingParams)
+        throw new Error(errors.MatchingParamNames);
     
     // TODO: Chcek for ambiguous routes
     routes.push({
