89acae7728aea2a7c45520b9d84085fdb00780be
Carl <carl@carlossus.com>
2015-11-01 08:23:37 +0800

    Move add logic into route function

--- src/route.js
+++ src/route.js
@@ -12,7 +12,22 @@ function route(options) {
         throw new Error(errors.NoPath);
     if (options.path.slice(0, 1) !== '/')
         options.path = '/' + options.path;
+    options.method = options.method.toUpperCase();
+    if (options.path === '/') {
+        var alreadyExists = routes.some(function (r) { return r.options.path === '/'; });
+        if (alreadyExists)
+            return;
+        routes.push({
+            options: options,
+            parts: []
+        });
+        return;
+    }
     var parts = routeParser(options.path);
+    var matchingParams = parts
+        .some(function (part) { return parts.filter(function (p) { return p.cast != null && p.part === part.part; }).length > 1; });
+    if (matchingParams)
+        throw new Error(errors.MatchingParamNames);
     var length = parts.length;
     if (length === 0)
         throw new Error(errors.InvalidPath);
--- src/route.ts
+++ src/route.ts
@@ -11,14 +11,33 @@ function route(options: Types.RouteOptions) {
 
     if (options.path.slice(0, 1) !== '/') options.path = '/' + options.path;
 
+    options.method = options.method.toUpperCase();
+
+    if (options.path === '/') {
+        var alreadyExists = routes.some(r => r.options.path === '/');
+        if (alreadyExists) return;
+
+        routes.push({
+            options,
+            parts: []
+        });
+        return;
+    }
+
     var parts = routeParser(options.path);
+
+    var matchingParams = parts
+        .some(part => parts.filter(p => p.cast != null && p.part === part.part).length > 1);
+
+    if (matchingParams)
+        throw new Error(errors.MatchingParamNames);
 
     var length = parts.length;
     if (length === 0) throw new Error(errors.InvalidPath);
-    
+
     var wildcardCount = parts.filter(p => p.type === 'wildcard').length;
     if (wildcardCount > 1) throw new Error(errors.InvalidWildCard);
-    
+
     var wildcardBeforeEnd = parts.some((part, index) => part.type === 'wildcard' && index !== length - 1);
     if (wildcardBeforeEnd) throw new Error(errors.InvalidWildCard);
 
