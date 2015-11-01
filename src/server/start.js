696b67aec8715c78567d2b621de3bc9654809cd7
Carl <carl@carlossus.com>
2015-11-01 22:18:17 +0800

    Add use internal config and allow host option

--- src/server/start.js
+++ src/server/start.js
@@ -1,8 +1,11 @@
 var config = require('../config');
 var server = require('./index');
+var connection = require('../connection');
 var DEFAULT_PORT = 2189;
+var DEFAULT_HOST = null;
 module.exports = function start(callback) {
-    var port = config.port || DEFAULT_PORT;
-    server.listen(port, callback);
+    var port = connection.port || config.port || DEFAULT_PORT;
+    var host = connection.host || config.host || DEFAULT_HOST;
+    server.listen(port, host, callback);
 };
 //# sourceMappingURL=start.js.map
--- src/server/start.ts
+++ src/server/start.ts
@@ -1,10 +1,13 @@
 import config = require('../config');
 import server = require('./index');
+import connection = require('../connection');
 
 const DEFAULT_PORT = 2189;
+const DEFAULT_HOST = null;
 
 export = function start(callback: () => void) {
-    var port = config.port || DEFAULT_PORT;
+    var port = connection.port || config.port || DEFAULT_PORT;
+    var host = connection.host || config.host || DEFAULT_HOST;
     
-    server.listen(port, callback);
+    server.listen(port, host, callback);
 }
