29b6b52701a8f4e5b2166a66f3babe0dcf35efb8
Carl Winkler <carl@carlossus.com>
2015-11-02 11:36:46 +0800

    Add getters to debug usage

--- debug/index.js
+++ debug/index.js
@@ -1,5 +1,12 @@
 var router = require('../src');
 var log = require('ls-logger');
+var users = [
+    { id: 1, name: 'seikho' },
+    { id: 2, name: 'lorezzed' },
+    { id: 3, name: 'dayson' }
+];
+var getId = function (id) { return users.filter(function (u) { return u.id === id; })[0]; };
+var getName = function (name) { return users.filter(function (u) { return u.name === name; })[0]; };
 router.start(function () { return log.info('Server started'); });
 router.route({
     method: 'GET',
@@ -17,13 +24,18 @@ router.route({
 });
 router.route({
     method: 'GET',
+    path: '/users',
+    handler: function (req, reply) { return reply(users); }
+});
+router.route({
+    method: 'GET',
     path: '/users/{id: number}',
-    handler: function (req, reply) { return reply("User id: " + req.params.id); }
+    handler: function (req, reply) { return reply(getId(req.params.id)); }
 });
 router.route({
     method: 'GET',
     path: '/users/{name: string}',
-    handler: function (req, reply) { return reply("Username: " + req.params.name); }
+    handler: function (req, reply) { return reply(getName(req.params.name)); }
 });
 router.route({
     method: 'GET',
--- debug/index.ts
+++ debug/index.ts
@@ -1,6 +1,15 @@
 import router = require('../src');
 import log = require('ls-logger');
 
+var users = [
+    { id: 1, name: 'seikho' },
+    { id: 2, name: 'lorezzed' },
+    { id: 3, name: 'dayson' }
+];
+
+var getId = (id: number) => users.filter(u => u.id === id)[0];
+var getName = (name: string) => users.filter(u => u.name === name)[0];
+
 router.start(() => log.info('Server started'));
 
 router.route({
@@ -21,14 +30,20 @@ router.route({
 
 router.route({
     method: 'GET',
+    path: '/users',
+    handler: (req, reply) => reply(users)
+});
+
+router.route({
+    method: 'GET',
     path: '/users/{id: number}',
-    handler: (req, reply) => reply(`User id: ${req.params.id}`)
+    handler: (req, reply) => reply(getId(req.params.id))
 });
 
 router.route({
     method: 'GET',
     path: '/users/{name: string}',
-    handler: (req, reply) => reply(`Username: ${req.params.name}`)
+    handler: (req, reply) => reply(getName(req.params.name))
 });
 
 router.route({
